054bcd3bc8a17ee3a9022f44d21ce46371507bfd
Biryukov-ala <abbs2305@gmail.com>
2021-07-19 20:25:38 +0700

    4.2. Тернарное сравнение [#189]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -3,11 +3,11 @@ package ru.job4j.condition;
 public class Max {
     public static int max(int left, int right) {
         boolean condition = true;
-        int result = left > right ? left : right;
-        return result = left > right ? left : right;
+       // int result = left > right ? left : right;
+        return left > right ? left : right;
     }
     public static void main(String[] args) {
-        int msg = Max.max(21,18);
+        int msg = Max.max(21,22);
         System.out.println(msg);
     }
 }
