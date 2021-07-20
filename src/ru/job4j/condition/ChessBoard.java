d510a78d0faaae1a51dff37ebb674937e827cd94
Biryukov-ala <abbs2305@gmail.com>
2021-07-20 21:23:22 +0700

    4.3.6. Math.abs [#373616 #179535]

--- src/ru/job4j/condition/ChessBoard.java
+++ src/ru/job4j/condition/ChessBoard.java
@@ -4,7 +4,7 @@ public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
         int rsl = 0;
         if (x1 >= 0 && x1 <= 7 && x2 >= 0 && x2 <= 7 && y1 >= 0 && y1 <= 7 && y2 >= 0 && y2 <= 7) {
-            if (Math.abs(y2 - y1) == Math.abs(y2 - y1)) {
+            if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
                 rsl = Math.abs(x2 - x1);
             }
             //тут то же самое написано что и во втором if, только если смещение фигур не одинаковое выводит 0
